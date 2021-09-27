d71dee68d1dfc8ccefb9f956c67512785d6f236d
karimou cisse <karimou.cisse1@gmail.com>
2021-09-27 11:58:17 +0200

    first commit

---
+++ jour01.js
@@ -0,0 +1,19 @@
+//01 - hello word
+console.log("hello word !");
+
+//02 - string
+var test = "My name is karimou";
+console.log(test);
+
+//03 - concatenation 
+var firstName = "karimou";
+console.log("nice to meet you "+ firstName);
+
+//04 - string lenght 
+var testLenght = "I'm very long !";
+console.log(testLenght.length);
+
+//05 - replace
+var food = "croissant is meh"
+//console.log(food.replace(mehfood));
+
