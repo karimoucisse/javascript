c367e68bc65a8b66f9b915c95d0426945a1d3837
karimou cisse <karimou.cisse1@gmail.com>
2021-09-28 15:11:04 +0200

    add jour3

--- jour2/index.js
+++ jour2/index.js
@@ -58,8 +58,7 @@ var score = 64;
 var password = "azerty";
 if(score>=limit && password.length>5){
     console.log("Everything is good");
-}
-else if(score>=limit || password.length>5) {
+}else if(score>=limit || password.length>5) {
     console.log("Something is good");
 }else {
     console.log("Nothing is good");
